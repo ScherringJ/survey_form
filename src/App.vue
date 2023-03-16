bc1f7fae00334b3624d3f8ba57467cde2878dee0
scherringj <scherring.jordan@gmail.com>
2023-03-16 15:59:49 +0100

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,126 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="mt-3 mx-sm-auto text-center card w-50 shadow-sm bg-body-tertiary rounded">
+    <div class="card-body">
+        
+        <Transition name="slide-fade" @after-leave="show = true">
+          <div class="text-center" v-if="show">
+            <p>
+              <strong>{{ questions[cptQuestion].question }}</strong>
+            </p>
+          </div>
+        </Transition>
+
+        <div class="text-center mb-3">
+          
+          <div class="d-inline mx-3">
+            Bad
+          </div>
+
+          <div class="form-check form-check-inline" v-for="index in 5" :key="index">
+            <input class="form-check-input" type="radio" :id=index  v-model="questions[cptQuestion].val" :value=index />
+            <label class="form-check-label" :for=index>{{ index }}</label>
+          </div>
+
+          <div class="d-inline me-4">
+            Excellent
+          </div>
+
+        </div>
+
+        <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+          <button class="btn btn-primary" type="button" @click="previousQuestion" v-if="!notPrevious"> Previous </button>
+          <button class="btn btn-primary" type="button" @click="nextQuestion" v-if="!isFinish"> Next </button>
+          <button class="btn btn-success" type="button" @click="submit" v-else> Submit </button>
+        </div>
+  
+    </div>
+
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, computed} from 'vue';
+import survey from './data/survey.json';
+import axios from 'axios';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const questions = ref(survey)
+
+const cptQuestion = ref(0)
+const show = ref(true)
+
+
+const isFinish = computed(() => {
+  return cptQuestion.value == questions.value.length - 1
+})
+
+const notPrevious = computed(() => {
+  return cptQuestion.value == 0
+})
+
+
+/**
+ * Go to the next question
+ */
+const nextQuestion = () => {
+  if (!isFinish.value) {
+    show.value=false
+    cptQuestion.value++
   }
 }
+
+
+/**
+ * Go to the previous question
+ */
+const previousQuestion = () => {
+  if (!notPrevious.value) {
+    show.value=false
+    cptQuestion.value--
+  }
+}
+
+
+/**
+ * Submit survey to the api 
+ */
+const submit = () => {
+  axios({
+    method: 'post',
+    url: '',
+    data: questions.value
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
 }
+
+.slide-fade-leave-active {
+  transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(-20px);
+  opacity: 0;
+}
+
+.slide-fade-enter-from {
+  transform: translateX(20px);
+  opacity: 0;
+}
+
+
 </style>
